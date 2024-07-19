fed8183b489b9340cec9275b7312b8de287b1c43
Smikkelman <grootaertk@gmail.com>
2024-07-19 18:39:54 +0200

    Initial commit fix
    Initial commit fix

--- Level/level.gd
+++ Level/level.gd
@@ -4,7 +4,7 @@ signal body_entered
 
 
 func _ready():
-	var enemy_spawn = load("res://Horde survival exp1/PlayerCharacter/EnemyCharacter.tscn")
+	var enemy_spawn = load("res://HordeSurvival/EnemyCharacter/EnemyCharacter.tscn")
 	var enemy = enemy_spawn.instantiate()
 	$Enemies.add_child(enemy)
 	#enemy.connect('collision',_on_enemy_collision)
